e6a3f1906bfae823bafe6669826d3eb3ffe183a7
TermAI <termai@bot.com>
2026-03-13 12:19:49 +0200

    fix: correct SnakqClient API for 1.21.4

--- src/main/java/com/snakq/client/SnakqClient.java
+++ src/main/java/com/snakq/client/SnakqClient.java
@@ -2,15 +2,32 @@ package com.snakq.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+import com.snakq.client.module.ModuleManager;
+import com.snakq.client.gui.ClickGUI;
 
 public class SnakqClient implements ClientModInitializer {
 
+    public static KeyBinding openGui;
+
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.startTickEvent().register(client -> {
-            // Initialize your mod here
-            ModuleManager.init();
+        ModuleManager.init();
+
+        openGui = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "Open ClickGUI",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_RIGHT_SHIFT,
+            "SnakqClient"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGui.wasPressed()) {
+                client.setScreen(new ClickGUI());
+            }
         });
     }
 }
