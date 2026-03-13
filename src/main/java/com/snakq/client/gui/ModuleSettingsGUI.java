4defafa54adceecd072c9aaef4e2583c9fbd2665
TermAI <termai@bot.com>
2026-03-13 12:09:50 +0200

    termai: fix attempt 3

---
+++ src/main/java/com/snakq/client/gui/ModuleSettingsGUI.java
@@ -0,0 +1,15 @@
+package com.snakq.client.gui;
+
+import net.minecraft.client.render.MatrixStack;
+import net.minecraft.client.render.item.ItemRenderer;
+import com.snakq.client.module.Module;
+import com.snakq.client.module.ModuleManager;
+
+public class ModuleSettingsGUI {
+    public ModuleSettingsGUI() {
+    }
+
+    public void render(MatrixStack matrices) {
+        // ...
+    }
+}
--- src/main/java/com/snakq/client/module/Module.java
+++ src/main/java/com/snakq/client/module/Module.java
@@ -1,37 +1,12 @@
 package com.snakq.client.module;
 
-public enum Category {
-    COMBAT,
-    MOVEMENT,
-    MISC
-}
-
 public abstract class Module {
-    private final String name;
-    private final Category category;
-    private boolean enabled;
+    private String moduleName;
 
-    public Module(String name, Category category) {
-        this.name = name;
-        this.category = category;
-        this.enabled = false;
+    public Module(String moduleName) {
+        this.moduleName = moduleName;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public abstract void init();
+    // Removed enum Category to a separate file
+    public abstract com.snakq.client.Category getCategory();
 }
